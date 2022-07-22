f25b48df32147d06a251c265e8f6bbea122c6244
Sebastián Caraballo <sebastiancaruy1@gmail.com>
2022-07-21 22:19:55 -0300

    Clase 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import img from "./images/time.jpeg"
+import NavBar from './components/NavBar';
 
 function App() {
+  const styles = { marginTop: "20px", color: "blue" }
+  styles.color = "green"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprendamos React con Coderhouse
-        </a>
-      </header>
+    <div>
+      <NavBar/>
+      <h1 className='title'>Hola Coderhouse</h1>
+      <br></br>
+      <p style={ styles }>Lorem ipsum</p>
+      <img width="200px" src={ img }></img>
+      <br></br>
+      <button onClick={ () => alert('hola')}>Click me</button>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,8 @@
+export default function NavBar(){
+  return(
+    <ul>
+      <li>Hola</li>
+      <li>Mundo</li>
+    </ul>
+  )
+}
