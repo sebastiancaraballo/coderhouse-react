7b239719585653e34cedf080e18c29e6ad44e175
Sebastián Caraballo <sebastiancaruy1@gmail.com>
2022-08-12 14:50:16 -0300

    Add item count to item detail

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,7 +1,12 @@
 import ReactMarkdown from 'react-markdown'
+import ItemCount from './ItemCount';
 
 function ItemDetail({item}) {
   const image = require(`../assets/images/${item.pictureUrl}`);
+
+  function handleItemCount(count) {
+    console.log(count);
+  }
 
   return (
     <div className="grid w-4/5 m-auto grid-cols-1 md:grid-cols-2">
@@ -15,7 +20,8 @@ function ItemDetail({item}) {
           {`$ ${item.price}`}
         </h3>
         <div className="border-t border-gray-400 my-3"></div>
-        <p className="mt-6">Product details:</p>
+        <ItemCount stock={item.stock} callback={handleItemCount} />
+        <p className="mt-3 mb-1">Product details:</p>
         <ReactMarkdown>
           {item.description}
         </ReactMarkdown>
