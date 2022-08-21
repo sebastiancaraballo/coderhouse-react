87ce45e61eac168a72d2b63e60d4903524f3d455
Sebastián Caraballo <sebastiancaruy1@gmail.com>
2022-08-20 22:43:50 -0300

    Cart context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './components/Header';
 import NavigationContainer from './components/NavigationContainer';
-import SecondaryHeader from './components/SecondaryHeader';
+import ShoppingCart from './components/ShoppingCart';
 import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer';
 import Footer from './components/Footer';
@@ -10,20 +10,23 @@ import {
   Routes,
   Route
 } from "react-router-dom";
+import {CartContextProvider} from './store/cartContext';
 
 function App() {
   return (
     <BrowserRouter>
-      <SecondaryHeader/>
-      <Header/>
-      <NavigationContainer/>
-      <Routes>
-        <Route path="categories/:categoryName" element={<ItemListContainer/>}></Route>
-        <Route path="item/:itemId" element={<ItemDetailContainer/>}></Route>
-        <Route path="/" element={<ItemListContainer/>}/>
-        <Route path="*" element={<h1>Not found</h1>} />
-      </Routes>
-      <Footer/>
+      <CartContextProvider>
+        <ShoppingCart/>
+        <Header/>
+        <NavigationContainer/>
+        <Routes>
+          <Route path="categories/:categoryName" element={<ItemListContainer/>}></Route>
+          <Route path="item/:itemId" element={<ItemDetailContainer/>}></Route>
+          <Route path="/" element={<ItemListContainer/>}/>
+          <Route path="*" element={<h1>Not found</h1>} />
+        </Routes>
+        <Footer/>
+      </CartContextProvider>
     </BrowserRouter>
   );
 }
